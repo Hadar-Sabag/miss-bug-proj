00e21a546d4fc9cff24e3a39b6ab253133ea44db
hadar <hadarsabag5@gmail.com>
2025-04-22 15:22:25 +0300

    Add

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 import express from 'express'
+
 const app = express()
+
 app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+app.listen(3031, () => console.log('Server ready at port 3031'))
+
+app.get('/api/bug', (req, res) => {res.send('bug: ')})
+app.get('/api/bug/save', (req, res) => {res.send('bug/save: ')})
+app.get('/api/bug/:bugId', (req, res) => {res.send('bug/:bugId')})
+app.get('/api/bug/:bugId/remove', (req, res) => {res.send('bug/:bugId/remove')})
